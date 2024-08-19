15f1d324fa75a75d22f2d359d0e27b76f9e7ef10
Arun2089 <arunlohar@gkmit.co>
2024-08-19 22:08:47 +0530

    Create route53.tf

---
+++ terraform/route53.tf
@@ -0,0 +1,13 @@
+resource "aws_route53_record" "alb_record" {
+  zone_id = Z01289041WN99TK7TRRUC 
+  name    = "jenkinstest.arunlohar.online"  
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app_lb.dns_name
+    zone_id                = aws_lb.app_lb.zone_id
+    evaluate_target_health = true
+  }
+
+  ttl = 60  
+}
