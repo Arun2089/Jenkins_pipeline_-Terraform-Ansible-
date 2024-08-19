8b5d5b1c0a3286769ac25900bd349de0bd0b4782
Arun Lohar <arunlohar@gkmit.co>
2024-08-19 19:46:38 +0530

    s

--- terraform/script.sh
+++ terraform/script.sh
@@ -1,6 +1,8 @@
 export public_ip_bastion=$(terraform output public_instance_ip | awk -F'"' '{print $2}') 
 export private_ip_main=$(terraform output private_instance_ip | awk -F'"' '{print $2}') 
 export pwd_path=$(pwd)
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
   echo "Host public_instance" > ~/.ssh/config
   echo "  HostName $public_ip_bastion" >> ~/.ssh/config
   echo "  Port 22" >> ~/.ssh/config
