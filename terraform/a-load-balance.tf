3a39630b896402ca2fc6292f6f2dce56fbcf7e51
Arun2089 <arunlohar@gkmit.co>
2024-08-21 18:53:17 +0530

    alb and routh53 edited

--- terraform/a-load-balance.tf
+++ terraform/a-load-balance.tf
@@ -20,7 +20,7 @@ resource "aws_lb_listener" "https" {
   port              = 443
   protocol          = "HTTPS"
 
- default_action {
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.jenkins.arn
   }
@@ -32,8 +32,6 @@ resource "aws_lb_listener" "https" {
     aws_lb_target_group.jenkins
   ]
 }
-
-
 
 # ALB HTTP Listener with redirection to HTTPS
 resource "aws_lb_listener" "http" {
@@ -66,7 +64,9 @@ resource "aws_lb_target_group" "jenkins" {
   vpc_id   = module.vpc.vpc_id
 
   health_check {
-    path                = "/"
+    path                = "/login"
+    port                = "8080"
+    protocol            = "HTTP"
     interval            = 30
     timeout             = 5
     healthy_threshold    = 2
@@ -78,7 +78,7 @@ resource "aws_lb_target_group" "jenkins" {
   }
 }
 
-# Attach EC2 instances to the Target Group
+# Attach EC2 instances to the Jenkins Target Group
 resource "aws_lb_target_group_attachment" "jenkins_instance_attachment" {
   count              = length(aws_instance.private_instance)
   target_group_arn   = aws_lb_target_group.jenkins.arn
@@ -88,5 +88,60 @@ resource "aws_lb_target_group_attachment" "jenkins_instance_attachment" {
   depends_on = [
     aws_lb_target_group.jenkins,
     aws_instance.private_instance
+  ]
+}
+
+# Target Group for React Application
+resource "aws_lb_target_group" "app_react" {
+  name     = "app-react-tg"
+  port     = 8081
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    port                = "8081"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold    = 2
+    unhealthy_threshold  = 2
+  }
+
+  tags = {
+    Name = "app-react-tg"
+  }
+}
+
+# Attach EC2 instances to the React Target Group
+resource "aws_lb_target_group_attachment" "app_react_instance_attachment" {
+  count              = length(aws_instance.private_instance)
+  target_group_arn   = aws_lb_target_group.app_react.arn
+  target_id          = aws_instance.private_instance[count.index].id
+  port               = 8081
+
+  depends_on = [
+    aws_lb_target_group.app_react,
+    aws_instance.private_instance
+  ]
+}
+
+# ALB Listener Rule for React Application
+resource "aws_lb_listener_rule" "react_rule" {
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_react.arn
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["react.arunlohar.online"]
+  }
+
+  listener_arn = aws_lb_listener.https.arn
+
+  depends_on = [
+    aws_lb_listener.https,
+    aws_lb_target_group.app_react
   ]
 }
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -11,3 +11,17 @@ resource "aws_route53_record" "alb_record" {
 
 
 }
+
+resource "aws_route53_record" "alb_record-2" {
+  zone_id = "Z01289041WN99TK7TRRUC"
+  name    = "react.arunlohar.online"  
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app_lb.dns_name
+    zone_id                = aws_lb.app_lb.zone_id
+    evaluate_target_health = true
+  }
+
+
+}
