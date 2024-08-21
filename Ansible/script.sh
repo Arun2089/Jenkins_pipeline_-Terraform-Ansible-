f8638bbf202a02b812f8875265b7bf34f5bfdf0d
Arun2089 <arunlohar@gkmit.co>
2024-08-21 20:54:24 +0530

    Delete Ansible/script.sh

--- Ansible/script.sh
+++
@@ -1,8 +0,0 @@
-export public_ip_bastion=$(terraform output public_instance_ip | awk -F'"' '{print $2}') 
-export private_ip_main=$(terraform output private_instance_ip | awk -F'"' '{print $2}') 
-
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-chmod 400 /var/lib/jenkins/workspace/Major_pipeline_1/terraform/Key.pem
-
-envsubst < ssh_config > ~/.ssh/config
